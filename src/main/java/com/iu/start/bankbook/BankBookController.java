94dadf4f96b0dd6c22dbcd4db09543c6800b0499
KimSeolHee <tjfgml64@nate.com>
2022-08-17 17:49:25 +0900

    service

--- src/main/java/com/iu/start/bankbook/BankBookController.java
+++ src/main/java/com/iu/start/bankbook/BankBookController.java
@@ -1,5 +1,49 @@
 package com.iu.start.bankbook;
 
-public class BankBookController {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/bankbook/*")
+public class BankBookController {
+	
+	@Autowired
+	private BankBookService bankBookService;	
+
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public void list(HttpServletRequest re) throws Exception{
+		List<BankBookDTO> ar= bankBookService.getList();
+		
+		re.setAttribute("list", ar);
+	}
+	
+//	public ModelAndView detail(Long bookNum) throws Exception {
+//		
+//	}
+//	
+//	public String add() {
+//		
+//	}
+//	
+//	public ModelAndView add(BankBookDTO dto) throws Exception {
+//		
+//	}
+//	
+//	public ModelAndView update(BankBookDTO dto, ModelAndView mv) throws Exception {
+//		
+//	}
+//	public ModelAndView update(BankBookDTO dto) throws Exception{
+//		
+//	}
+//	
+//	public ModelAndView delete(BankBookDTO dto) throws Exception {
+//		
+//	}
 }
